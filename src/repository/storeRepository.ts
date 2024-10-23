03e094098b8ace4468d5f0e999b7b02aaddd8f45
Scalu-Neo <codigomatrix10@gmail.com>
2024-10-23 18:51:57 -0300

    Operações de Crud em Store

--- src/repository/storeRepository.ts
+++ src/repository/storeRepository.ts
@@ -1,33 +1,6 @@
 import { IStore, StoreModel } from "../model/storeModel";
-import { AddressModel } from "../model/addressModel";
-import asyncHandler from "../utils/asyncHandler";
+import GenericRepository from "../repository/genericRepository.ts"
 
-class StoreRepository {
+const storeRepository = new GenericRepository<IStore>(StoreModel);
 
-    findAll = asyncHandler(async():Promise<IStore[]> => {
-        return await StoreModel.find();
-    });
-
-    findById = asyncHandler(async(cep:string):Promise<IStore | null> => {
-        return await StoreModel.findOne({cep});
-    });
-
-    create = asyncHandler(async(store:IStore):Promise<IStore> => {
-        const newStore = new StoreModel(store);
-        return await newStore.save();
-    });
-
-    update = asyncHandler(async(id:string, store:IStore):Promise<IStore | null> => {
-        return await StoreModel.findByIdUpdate(id, store, {
-            new:true,
-            runValidators: true,
-        });
-    });
-
-    delete = asyncHandler(async(id:string):Promise<IStore | null> => {
-        return await StoreModel.findByIdDelete({id}); 
-    });
-
-}
-
-export default new StoreRepository();
+export default storeRepository;
